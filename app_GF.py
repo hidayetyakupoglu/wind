e130b9a4abd571d1bb7d0926c7715ada027ec551
hidayetyakupoglu <80601785+hidayetyakupoglu@users.noreply.github.com>
2025-06-24 01:58:54 +0300

    Update app_GF.py

--- app_GF.py
+++ app_GF.py
@@ -33,7 +33,7 @@ selected_features = [
 @st.cache_resource
 def load_model(path):
     try:
-        model = load_model("cnn_bigru_online_kalman.h5")
+        model = load_model(path)
         return model
     except Exception as e:
         st.error(f"Model yüklenirken hata: {e}")
